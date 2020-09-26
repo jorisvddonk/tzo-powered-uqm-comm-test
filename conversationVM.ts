c7d2aa951eb5febb6322cbc46e03b097378c4059
Joris van de Donk <jorisvddonk@gmail.com>
2020-09-26 18:34:08 +0200

    add ability to play audio (basic, without text timings yet)

--- conversationVM.ts
+++ conversationVM.ts
@@ -1,16 +1,20 @@
-import { TzoVMState } from "tzo";
+import { getStackParams, Stack, TzoVMState } from "tzo";
 import { QuestVM, Choice } from "questmark";
-import { parseTextLocalizationFile } from "uqm-files-parsers";
+import { parseTextLocalizationFile, parseTextSynchronizationFile } from "uqm-files-parsers";
 import fs from "fs";
+import path from "path";
+import { LocalizationsMap, SynchronizationsMap } from "uqm-files-parsers/dist/interfaces";
 
 export class ConversationVM extends QuestVM {
   displayString = "";
   options: Choice[] = [];
   optionPromiseCallback: (number) => void = null;
   selectedOptionIndex = 0;
-  translations = null;
+  translations: LocalizationsMap = null;
+  texttimings: SynchronizationsMap = null;
+  r;
 
-  constructor(conversationVMPath: string, translationsFilePath: string) {
+  constructor(r, conversationVMPath: string, translationsFilePath: string, synchronizationFilePath: string) {
     super((body => {
       this.displayString = `${this.displayString}${this.localize(body)}`
     }), async choices => {
@@ -19,8 +23,17 @@ export class ConversationVM extends QuestVM {
       return new Promise((resolve) => {
         this.optionPromiseCallback = resolve;
       });
+    }, {
+      playAudio: (stack: Stack) => {
+        const [audioFile] = getStackParams("loadImage", ["string"], stack) as [string];
+        const p = path.join(path.dirname(synchronizationFilePath), audioFile);
+        const s = r.LoadSound(p);
+        r.PlaySound(s);
+      }
     });
+    this.r = r;
     this.translations = parseTextLocalizationFile(fs.readFileSync(translationsFilePath).toString());
+    this.texttimings = parseTextSynchronizationFile(fs.readFileSync(synchronizationFilePath).toString());
     this.loadVMState(JSON.parse(fs.readFileSync(conversationVMPath).toString()) as TzoVMState);
     this.run();
   }

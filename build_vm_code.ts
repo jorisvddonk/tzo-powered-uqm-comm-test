aaba36e5e903c636cc3dc4805238c22d67b4b459
Joris van de Donk <jorisvddonk@gmail.com>
2020-09-26 14:13:48 +0200

    add YO_YO

--- build_vm_code.ts
+++ build_vm_code.ts
@@ -25,7 +25,14 @@ graphics.animations.forEach(anim => {
     pushNumber(0),
     pushString(`anim_${anim.name}_index`),
     invokeFunction("setContext")
-  ])
+  ]);
+  if (anim.type === AnimationType.YO_YO) {
+    b.add([
+      pushNumber(1),
+      pushString(`anim_${anim.name}_yy_direction`),
+      invokeFunction("setContext")
+    ])
+  }
 });
 
 // pause the VM!
@@ -68,6 +75,50 @@ graphics.animations.forEach(anim => {
       invokeFunction('randInt'),
       pushString(`anim_${anim.name}_index`),
       invokeFunction("setContext")
+    ])
+  } else if (anim.type === AnimationType.YO_YO) {
+    // add or subtract to the index until we reach the end, then reverse!
+    b.add([
+      // are we moving forwards and at the last frame?
+      pushString(`anim_${anim.name}_yy_direction`),
+      invokeFunction("getContext"),
+      pushNumber(1),
+      invokeFunction("eq"), // if 1: we are moving forward!
+      pushString(`anim_${anim.name}_index`),
+      invokeFunction("getContext"),
+      pushNumber(anim.frames.length - 1),
+      invokeFunction("eq"), // if 1: we are at last frame!
+      invokeFunction("and"), // if 1: we are moving forward AND at last frame
+      invokeFunction("jgz"),
+      invokeFunction("{"),
+      pushNumber(-1), // reverse
+      pushString(`anim_${anim.name}_yy_direction`),
+      invokeFunction("setContext"),
+      invokeFunction("}"),
+      // are we moving backwards and at the first frame?
+      pushString(`anim_${anim.name}_yy_direction`),
+      invokeFunction("getContext"),
+      pushNumber(-1),
+      invokeFunction("eq"), // if 1: we are moving backward!
+      pushString(`anim_${anim.name}_index`),
+      invokeFunction("getContext"),
+      pushNumber(0),
+      invokeFunction("eq"), // if 1: we are at first frame!
+      invokeFunction("and"), // if 1: we are moving backward AND at first frame
+      invokeFunction("jgz"),
+      invokeFunction("{"),
+      pushNumber(1), // reverse
+      pushString(`anim_${anim.name}_yy_direction`),
+      invokeFunction("setContext"),
+      invokeFunction("}"),
+      // add yy direction to index
+      pushString(`anim_${anim.name}_yy_direction`),
+      invokeFunction("getContext"),
+      pushString(`anim_${anim.name}_index`),
+      invokeFunction("getContext"),
+      invokeFunction("+"),
+      pushString(`anim_${anim.name}_index`),
+      invokeFunction("setContext"),
     ])
   }
 });

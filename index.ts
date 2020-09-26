604e08ddab7d47cd2673dfa4be67dd4cf34b905f
Joris van de Donk <jorisvddonk@gmail.com>
2020-09-26 16:32:20 +0200

    simple QuestVM integration

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import r from "raylib";
 import { getStackParams, Stack, TzoVMState, VM } from "tzo";
 import fs from "fs";
 import { TextureVM } from "./textureVM";
+import { QuestVM } from "questmark";
 
 
 const base = "./asking-about-flowers/comm/spathi";
@@ -14,12 +15,21 @@ const tvm = new TextureVM(r, base);
 tvm.loadVMState(JSON.parse(fs.readFileSync("./anim.json").toString()) as TzoVMState);
 tvm.run(); // start initiation process!
 
+let displayString = "";
+const qvm = new QuestVM((body => displayString = `${displayString}${body}`), async choices => {
+  return new Promise((resolve, reject) => {
+    // TODO: implement.
+  });
+});
+qvm.loadVMState(JSON.parse(fs.readFileSync("./speech.json").toString()) as TzoVMState);
+qvm.run();
 
 while (!r.WindowShouldClose()) {
   r.BeginDrawing();
   r.ClearBackground(r.RAYWHITE);
   tvm.run(); // resume VM! This will draw the next frame...
   r.DrawText(JSON.stringify(tvm.context, null, 2), 20, 20, 5, r.BLACK)
+  r.DrawText(displayString, 300, 10, 10, r.BLUE);
   r.EndDrawing()
 }
 r.CloseWindow()        // Close window and OpenGL context

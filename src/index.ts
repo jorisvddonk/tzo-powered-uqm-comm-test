ba86690b51e2760c285ac40a84f50ff761fc41ca
joris <jorisvddonk@gmail.com>
2020-10-03 15:13:13 +0200

    display error stacktraces

--- src/index.ts
+++ src/index.ts
@@ -13,12 +13,19 @@ r.InitWindow(screenWidth, screenHeight, "Tzo powered Animation Test");
 r.SetTargetFPS(30);
 r.InitAudioDevice();
 
+let err: Error = undefined;
+const errHandler = e => {
+  console.error(e);
+  err = e;
+}
+
 const tvm = new TextureVM(r, base);
 tvm.loadVMState(JSON.parse(fs.readFileSync("./anim.json").toString()) as TzoVMState);
 tvm.run(); // start initiation process!
 
 let debug = false;
 const cvm = new ConversationVM(r, `./speech.json`, `${base}/${alien_name}.json`);
+cvm.eventBus.on('error', errHandler);
 const font = r.LoadFont(`./asking-about-flowers/fonts/${alien_name}.fon.png`);
 
 const music = r.LoadMusicStream(`${base}/${alien_name}.mod`);
@@ -30,15 +37,20 @@ const interv = setInterval(() => {
       debug = !debug;
     }
 
-    if (r.IsKeyPressed(r.KEY_DOWN)) {
-      cvm.selectNextOption();
-    }
-    if (r.IsKeyPressed(r.KEY_UP)) {
-      cvm.selectPreviousOption();
-    }
+    try {
+      if (r.IsKeyPressed(r.KEY_DOWN)) {
+        cvm.selectNextOption();
+      }
+      if (r.IsKeyPressed(r.KEY_UP)) {
+        cvm.selectPreviousOption();
+      }
 
-    if (r.IsKeyPressed(r.KEY_ENTER) || r.IsKeyPressed(r.KEY_SPACE)) {
-      cvm.commitToSelectedOption();
+      if (r.IsKeyPressed(r.KEY_ENTER) || r.IsKeyPressed(r.KEY_SPACE)) {
+        cvm.commitToSelectedOption();
+      }
+    } catch (e) {
+      errHandler(e);
+      cvm.quit();
     }
 
     r.BeginDrawing();
@@ -47,7 +59,12 @@ const interv = setInterval(() => {
     r.UpdateMusicStream(music);
 
     // Draw alien comm screen:
-    tvm.run(); // resume VM! This will draw the next frame...
+    try {
+      tvm.run(); // resume VM! This will draw the next frame...
+    } catch (e) {
+      errHandler(e);
+      tvm.quit();
+    }
 
     // Draw alien conversation and player options:
     r.DrawTextRec(font, cvm.getDisplayText(), r.Rectangle(5, 5, r.GetScreenWidth() - 5, r.GetScreenHeight() - 5), 25, -2, true, r.WHITE);
@@ -64,6 +81,11 @@ const interv = setInterval(() => {
 
       r.DrawText(JSON.stringify(tvm.stack), 20, 400, 5, r.WHITE);
       r.DrawText(JSON.stringify(cvm.stack), 400, 400, 5, r.WHITE);
+    }
+
+    // error drawing:
+    if (err !== undefined) {
+      r.DrawText(err.toString(), 20, 1, 8, r.RED);
     }
 
     r.EndDrawing();
